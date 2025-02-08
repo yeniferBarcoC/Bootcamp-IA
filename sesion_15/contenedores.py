89d9d1fa21e39aba53bc139fd91bc93109f88483
yeniferBarcoC <yenibarcas@gmail.com>
2025-02-07 22:37:03 -0500

    Sesión 15: Se comenzó con la creación de la carpeta 'sesion_15'. Luego, se procede a realizar la actividad de Contenedores en python dando como resultado la creación del archivo 'contenedores.py' y se desarrolló hasta el contenedor tipo 'Lista'. Aun falta finalizar el mencionado contenedor

---
+++ sesion_15/contenedores.py
@@ -0,0 +1,78 @@
+"""
+------------------------------ACTIVIDAD 4. CONTENEDORES EN PYTHON------------------------------
+--------------------------------Modulo1. Unidad 2. Lección 1.----------------------------------
+
+Contiene: * Presentacion de estructuras de datos cmo listas, tuplas, conjuntos, diccionarios.
+          * Operaciones y métodos básicos para manejar los contenedores.
+
+Autor: Yenifer Barco C"""
+          
+#------------------------------------------Página 3---------------------------------------------
+
+"""--------------------------------------------------------------------------------------------|
+|1. LISTAS: es un contenedor flexible y mutable que permite almacenar una colecció n           |
+|ordenada de elementos. Las listas van en corchetes'[]'.                                       |
+|----------------------------------------------------------------------------------------------|
+"""
+# 1.1. Declaración de una lista
+pokemones_de_ash = ["Pikachu","Carterpie", "Pidgey", "Charmander", "Squirtle"]
+print("\n---------------DECLARACION DE UNA LISTA:------------------- ")
+print(pokemones_de_ash)
+print(pokemones_de_ash[0])
+
+# 1.2. Indice y Acceso a los elementos de la lista: los elementos de una lista se numeran 
+# desde 0 en adelante. Se accede a un elemento mediante su índice.
+print("\n---------------INDICE Y ACCESO A LOS ELEMENTOS DE LA LISTA:------------------- ")
+primer_pokemon = pokemones_de_ash[0]
+print(primer_pokemon)
+segundo_pokemon = pokemones_de_ash[1]
+print("El segundo pokemon de ash es {segundo_pokemon}")
+
+#------------------------------------------Página 4---------------------------------------------
+
+# 1.3. Longitud de la lista: Se puede obtener la longitud de una lista con la funcion len().
+# la longitud de una lista es la cantidad de elementos que contiene una lista
+longitud = len(pokemones_de_ash)
+print("\n---------------LONGITUD DE LA LISTA:------------------- \n Cantidad de pokemones de ash:",longitud)
+
+# 1.4. Modificacion de un elemento de la lista: Se puede modificar un elemento de una 
+# lista mediante su indice
+pokemones_de_ash[3] = "Tauros"
+print("\n---------------MODIFICACION DE UN ELEMENTO DE LA LISTA:------------------- \n",pokemones_de_ash)
+
+#------------------------------------------Página 5---------------------------------------------
+# 1.5 OPERACIONES COMUNES EN LISTAS
+
+# 1.5.1 Agregar un elemento al final de la lista: Se puede agregar un elemento
+# al final de una lista con la funcion append()
+pokemones_de_ash.append("Pidgeotto")
+print("\n---------------AGREGAR UN ELEMENTO AL FINAL DE LA LISTA:------------------- \n",pokemones_de_ash)  
+
+# 1.5.2 Eliminar un elemento de la lista: Se puede eliminar un elemento de una
+# lista con la funcion remove()
+pokemones_de_ash.remove("Pidgey")
+print("\n---------------ELIMINAR UN ELEMENTO DE LA LISTA:------------------- \n",pokemones_de_ash)
+
+# 1.5.3 Extender una lista con otra: Se puede extender una lista con otra con la
+# funcion extend()
+pokemones_de_ash.extend(["Pidgeot","Rattata"])
+print("\n---------------EXTENDER UNA LISTA CON OTRA:------------------- \n",pokemones_de_ash)
+
+#------------------------------------------Página 6---------------------------------------------
+# Slicing (Rebanado): Se puede ontener un pedazo de la lista mediante el uso de slicing
+sublista = pokemones_de_ash[1:3]
+print("\n---------------Slicing (Rebanado):------------------- \n",sublista)
+
+# Inclusión de elementos: se puede verificar si un elemento se encuentra en una lista con el
+# operador in
+existe = "Tauros" in pokemones_de_ash
+print("\n---------------Inclusión de elementos:------------------- \n", "Tauros" in pokemones_de_ash)
+
+#------------------------------------------Página 7---------------------------------------------
+
+"""--------------------------------------------------------------------------------------------|
+|2. TUPLAS: es otro tipo de contenedor, similar a una lista, pero con la diferencia principal  | 
+|de que es inmutable, lo que significa que no puedes modificar su contenido después de haber   |
+|sido creada. Las tuplas van en parentesis'()'.                                                |
+|----------------------------------------------------------------------------------------------|
+"""
