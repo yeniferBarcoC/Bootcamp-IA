406d026d2fed396ec8cc04b3c8f0717af0245aec
yeniferBarcoC <yenibarcas@gmail.com>
2025-02-08 12:37:51 -0500

    Sesión 16: Se procede continuar con la actividad de Contenedores en python dando continuidad en el archivo 'contenedores.py' y se desarrolló hasta el contenedor tipo 'Lista'. Aun falta finalizar los demás tipos de contenedores

--- sesion_15/contenedores.py
+++ sesion_15/contenedores.py
@@ -12,17 +12,20 @@ Autor: Yenifer Barco C"""
 """--------------------------------------------------------------------------------------------|
 |1. LISTAS: es un contenedor flexible y mutable que permite almacenar una colecció n           |
 |ordenada de elementos. Las listas van en corchetes'[]'.                                       |
+|Las listas son herramientas versatiles en python y se utilizan comunmente para almacenar y    |
+|manipular colecciones de datos                                                                |                    
 |----------------------------------------------------------------------------------------------|
 """
 # 1.1. Declaración de una lista
 pokemones_de_ash = ["Pikachu","Carterpie", "Pidgey", "Charmander", "Squirtle"]
-print("\n---------------DECLARACION DE UNA LISTA:------------------- ")
+print("\n--------------------------LISTA---------------------------- ")
+print("\n---------------Declaracion de una lista:------------------- ")
 print(pokemones_de_ash)
 print(pokemones_de_ash[0])
 
 # 1.2. Indice y Acceso a los elementos de la lista: los elementos de una lista se numeran 
 # desde 0 en adelante. Se accede a un elemento mediante su índice.
-print("\n---------------INDICE Y ACCESO A LOS ELEMENTOS DE LA LISTA:------------------- ")
+print("\n---------------Indice y acceso a los elementos de una lista:------------------- ")
 primer_pokemon = pokemones_de_ash[0]
 print(primer_pokemon)
 segundo_pokemon = pokemones_de_ash[1]
@@ -32,13 +35,13 @@ print("El segundo pokemon de ash es {segundo_pokemon}")
 
 # 1.3. Longitud de la lista: Se puede obtener la longitud de una lista con la funcion len().
 # la longitud de una lista es la cantidad de elementos que contiene una lista
-longitud = len(pokemones_de_ash)
-print("\n---------------LONGITUD DE LA LISTA:------------------- \n Cantidad de pokemones de ash:",longitud)
+cantidad_pokemones = len(pokemones_de_ash)
+print("\n--------------Longitud de la lista:------------------- \n Cantidad de pokemones de ash:",cantidad_pokemones)
 
 # 1.4. Modificacion de un elemento de la lista: Se puede modificar un elemento de una 
 # lista mediante su indice
 pokemones_de_ash[3] = "Tauros"
-print("\n---------------MODIFICACION DE UN ELEMENTO DE LA LISTA:------------------- \n",pokemones_de_ash)
+print("\n---------------Modificacion de un elemento de la lista:------------------- \n",pokemones_de_ash)
 
 #------------------------------------------Página 5---------------------------------------------
 # 1.5 OPERACIONES COMUNES EN LISTAS
@@ -46,27 +49,33 @@ print("\n---------------MODIFICACION DE UN ELEMENTO DE LA LISTA:----------------
 # 1.5.1 Agregar un elemento al final de la lista: Se puede agregar un elemento
 # al final de una lista con la funcion append()
 pokemones_de_ash.append("Pidgeotto")
-print("\n---------------AGREGAR UN ELEMENTO AL FINAL DE LA LISTA:------------------- \n",pokemones_de_ash)  
+print("\n---------------Agregar un elemento al final de la lista:------------------- \n",pokemones_de_ash)  
 
 # 1.5.2 Eliminar un elemento de la lista: Se puede eliminar un elemento de una
 # lista con la funcion remove()
 pokemones_de_ash.remove("Pidgey")
-print("\n---------------ELIMINAR UN ELEMENTO DE LA LISTA:------------------- \n",pokemones_de_ash)
+print("\n---------------Eliminar un elemento de la lista:------------------- \n",pokemones_de_ash)
 
 # 1.5.3 Extender una lista con otra: Se puede extender una lista con otra con la
 # funcion extend()
-pokemones_de_ash.extend(["Pidgeot","Rattata"])
-print("\n---------------EXTENDER UNA LISTA CON OTRA:------------------- \n",pokemones_de_ash)
+pokemones_de_ash.extend(["Pidgeot","Rattata",17,40])
+print("\n---------------Extender una lista con otra:------------------- \n",pokemones_de_ash)
+
+#RETO: Agregar los pokemosnes de Brock a la lista de pokemones de Ash
+pokemones_de_brock = ["Onix", "Subat", "Crobat"]
+pokemones_de_ash.extend(pokemones_de_brock)
+print("RETO: Pokemones de Brock en Ash: ", pokemones_de_ash)
 
 #------------------------------------------Página 6---------------------------------------------
-# Slicing (Rebanado): Se puede ontener un pedazo de la lista mediante el uso de slicing
-sublista = pokemones_de_ash[1:3]
-print("\n---------------Slicing (Rebanado):------------------- \n",sublista)
+# 1.5.4 Slicing (Rebanado): Se puede ontener un pedazo de la lista mediante el uso de slicing
+batalla = pokemones_de_ash[1:3]
+print("\n---------------SLICING(Recortando ó Rebanado):------------------- \n",batalla)
 
-# Inclusión de elementos: se puede verificar si un elemento se encuentra en una lista con el
+# 1.5.5 Inclusión de elementos: se puede verificar si un elemento se encuentra en una lista con el
 # operador in
 existe = "Tauros" in pokemones_de_ash
-print("\n---------------Inclusión de elementos:------------------- \n", "Tauros" in pokemones_de_ash)
+print("\n---------------Inclusion de elementos:------------------- \n", "Tauros" in pokemones_de_ash)
+
 
 #------------------------------------------Página 7---------------------------------------------
 
@@ -76,3 +85,15 @@ print("\n---------------Inclusión de elementos:------------------- \n", "Tauros
 |sido creada. Las tuplas van en parentesis'()'.                                                |
 |----------------------------------------------------------------------------------------------|
 """
+# 2.1. Declaración de una Tupla
+mi_tupla = (1,2,3,'a','b','c')
+print("\n---------------DECLARACION DE UNA TUPLA:------------------- ")
+
+# 2.2. Indice y Acceso a los elementos: los elementos de una tupla también se numeran desde 0 en adelante.
+primer_elemento = mi_tupla[0]
+segundo_pokemon = mi_tupla[1]
+print("\n---------------INDICE Y ACCESO A LOS ELEMENTOS DE UNA TUPLA:------------------- ")
+
+# 2.3 Longitud de una Tupla: 
+longitud = len(mi_tupla)
+print("\n---------------LONGITUD DE LA TUPLA:------------------- \n Cantidad de pokemones de ash:",longitud)
