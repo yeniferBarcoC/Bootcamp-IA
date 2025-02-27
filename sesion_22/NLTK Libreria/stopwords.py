d2ce365d4ef2b23dc91b4b50700e8fbca2844fa6
yeniferBarcoC <yenibarcas@gmail.com>
2025-02-26 22:18:05 -0500

    Se modifico el archivo de resumen de clases donde esta todo los temas que se vieron en clase

---
+++ sesion_22/NLTK Libreria/stopwords.py
@@ -0,0 +1,12 @@
+#Importamos la libreria NLTK que nos ayuda a trabajar
+#con lenguaje natural en python
+import nltk
+
+#Desde NLTK importamos la libreria stopwords que son comunes en
+#nuestro lenguaje
+from nltk.corpus import stopwords
+
+#Descargamos la lista de palabras vacias o stopwords, si no
+# esta disponoible en nuestra computadora
+nltk.download('stopwords')
+
